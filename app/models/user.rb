daa0a42931aa82bd46cb682e0df5f048bbe887a5
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-26 17:39:16 -0400

    added assoiciations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_secure_password
+
+    # Username must be present and unique
+    # Email must be present and unique
+    # Password must contain 8 or more characters
+    # Image is optional
+
+    has_one :nomination_list
+    has_many :movies, through: :nomination_list
 end
