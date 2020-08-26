a1e774ca06b9613c4e1f38392af1c513d9b30207
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-26 17:42:40 -0400

    updated schema with nomination list data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_192827) do
+ActiveRecord::Schema.define(version: 2020_08_26_214013) do
+
+  create_table "nomination_lists", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_nomination_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -21,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_08_26_192827) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "nomination_lists", "users"
 end
