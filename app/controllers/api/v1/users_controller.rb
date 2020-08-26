70a18ef73c04ddbddb13468f097ffe2f99cf4ffb
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-26 17:39:00 -0400

    added index action to user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,15 @@
-class API::V1::UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+
+    def index 
+        users = User.all.order('created_at')
+        render json: {
+            users: UserSerializer.new(users).serializable_hash
+        }
+    end
+
+    # Users can search for movies
+    # Users can nominate movies 
+    # Users can delete movies from the nomination list
+    # Users can review nomination list
+
 end
