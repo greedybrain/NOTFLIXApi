2c0a546a5d3a66e7b307c3b05eb7bccf1a375c75
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-26 23:01:13 -0400

    added signup action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,27 @@
 class RegistrationsController < ApplicationController
+
+    def signup 
+        user = User.new(user_params)
+        if user.save 
+            session[:user_id] = user.id
+            render json: {
+                user: UserSerializer.new(user).serializable_hash,   
+                logged_in: true,
+                status: :created
+            }
+        else {
+            render json: {
+                status: 401,
+                message: "Something went wrong"
+            }
+        }
+        end
+    end
+
+    private
+
+    def user_params 
+        params.require(:user).permit(:username, :email, :password, :image)
+    end
+
 end
