0e49372b1e9a9c6ab943d1b5afef3f5c39896ed5
Naya Willis <whatyoucodingboutwillis@MacBook-Pro.localdomain>
2020-08-27 17:05:00 -0400

    added show action to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,18 @@
 class Api::V1::UsersController < ApplicationController
 
     def index 
-        users = User.all.order('created_at')
+        users = User.order('created_at')
         render json: {
             users: UserSerializer.new(users).serializable_hash
         }
     end
 
-    # Users can search for movies
-    # Users can nominate movies 
-    # Users can delete movies from the nomination list
-    # Users can review nomination list
+    def show 
+        user = User.find(params[:id])
+        render json: {
+            user: UserSerializer.new(user),
+            message: "We found #{user.username}"
+        }
+    end
 
 end
