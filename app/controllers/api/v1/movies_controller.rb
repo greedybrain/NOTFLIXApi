4cb3b3c6c5f1779cd83618545e0efb977cccaf29
Naya Willis <whatyoucodingboutwillis@Nayas-MacBook-Pro.local>
2020-08-27 21:36:38 -0400

    added comments to movie controller

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::MoviesController < ApplicationController
 
     def index 
+        # GET ALL MOVIES ORDERED FROM MOST RECENT TO OLDEST
         movies = Movie.order('created_at')
         render json: {
             movies: MovieSerializer.new(movies)
@@ -8,6 +9,7 @@ class Api::V1::MoviesController < ApplicationController
     end
 
     def create 
+        # HANDLING ADDING A MOVIE TO A USERS NOMINATIONS
         if params[:user_id]
             user = User.find(params[:user_id])
             if user&.movies.length >= 5
@@ -26,6 +28,19 @@ class Api::V1::MoviesController < ApplicationController
         end
     end
 
+    def destroy 
+        if params[:user_id]
+            user = User.find(params[:user_id])
+            movie = user.movies.find(params[:id])
+            if movie.destroy
+                render json: {
+                    movie: MovieSerializer.new(movie).serializable_hash,
+                    message: "Movie removed from nomination list"
+                }
+            end
+        end
+    end
+
     # Users can search for movies
     # Users can nominate movies 
     # Users can delete movies from the nomination list
@@ -33,7 +48,8 @@ class Api::V1::MoviesController < ApplicationController
 
     private 
 
-    def movie_params 
+    def movie_params
+        # SHOWS PERMITTED PARAMS FOR A MOVIE 
         params.permit(:title, :release_year, :plot, :poster)
     end
 
