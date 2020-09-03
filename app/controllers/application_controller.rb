0618539a3bebb12f851ce91078a41086a9b45837
Naya Willis <whatyoucodingboutwillis@MacBook-Pro.localdomain>
2020-09-03 15:26:13 -0400

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery
+    skip_before_action :verify_authenticity_token
+    before_action :current_user
     
     def current_user 
-        # ????
         if session[:user_id]
             @current_user = User.find(session[:user_id])
         end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    # before_action :current_user, only: [:logged_in]
 
     def login
         # HANDLES LOGGING IN USER 
@@ -19,18 +20,24 @@ class SessionsController < ApplicationController
     end
 
     # NEEDS FIXING
-    # def logged_in
-    #     if @current_user
-    #         render json: {
-    #             logged_in: true,
-    #             user: @current_user
-    #         }
-    #     else
-    #         render json: {
-    #             logged_in: false
-    #         }
-    #     end
-    # end
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: {
+                    id: @current_user.id,
+                    username: @current_user.username,
+                    email: @current_user.email,
+                    image: @current_user.image,
+                    movies: @current_user.movies
+                }
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+    end
 
     def logout 
         # HANDLE LOGGING USER OUT 
