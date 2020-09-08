fa440c1d7cb111908071dabe12d21162d1592a4f
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-08 09:45:27 -0400

    changed helper method to set_current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery
     skip_before_action :verify_authenticity_token
-    before_action :current_user
-    
-    def current_user 
+    before_action :set_current_user
+
+    def set_current_user 
         if session[:user_id]
             @current_user = User.find(session[:user_id])
         end
     end
-
 end
