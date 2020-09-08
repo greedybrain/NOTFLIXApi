461f68ec579b1d0968f67efc33aa4f18174482a3
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-08 09:49:05 -0400

    created cust meth movie_limit_reached

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,22 @@ class User < ApplicationRecord
     # Image is optional
 
     has_many :movies, dependent: :destroy
+
+    def movie_limit_reached?
+        self&.movies.length >= 5
+    end
+
+    def saved_movies_contain_movie?(movie)
+        found_movie = self.movies.find {|mov| mov.title == movie.title}
+        binding.pry
+        found_movie.nil?
+    end
+
+    # f user&.movies.length >= 5
+    #             render json: {
+    #                 user_movies: MovieSerializer.new(user.movies).serializable_hash,
+    #                 message: "You can only nominate 5 movies"
+    #             }
+
+
 end
