c3fdb3fbc830873e67a422593a0d1169d8e6c365
WhatYouCodingBoutWillis <whatyoucodingboutwillis@WhatYouCodingBoutWilliss-MacBook-Pro.local>
2020-09-08 09:49:13 -0400

    null

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,4 @@
 class MovieSerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :actors, :genre, :language, :country, :runtime, :plot, :poster,  :release_year, :imdb_rating, :production
-
 end
