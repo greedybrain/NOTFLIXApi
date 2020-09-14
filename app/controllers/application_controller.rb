b6ea176adbece32c85cd3d19de71465861c26615
WhatYouCodingBoutWillis <whatyoucodingboutwillis@Nayas-MBP.localdomain>
2020-09-14 10:55:32 -0400

    changed helper meth setcurrentuser to current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
     skip_before_action :verify_authenticity_token
-    before_action :set_current_user
+    helper_method :current_user
 
-    def set_current_user 
+    def current_user 
         if session[:user_id]
             @current_user = User.find(session[:user_id])
         end
